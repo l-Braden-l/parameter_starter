342afe8a090cfe7ba2fc8a91cd956603a7423643
braden <leachbr787@northwested.org>
2024-11-18 14:04:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,17 @@
 # Default Values for Parameters Practice
 
 # 8-3 (T-Shirt)
-    #positional 
+#positional 
 def make_shirt(size, text):
     print(f'The size of your shirt is {size} and it says {text} on the front.')
 
 make_shirt('small', 'Happy Halloween!')
-    #keyword
+
+# Keyword
 def make_shirt(size, text):
-    print(f'The size of your shirt is {size} and it says {text} on the front.')
+    print(f'The size of your shirt is {size} and it says {text} on the front.\n')
 
 make_shirt(size= 'large', text='Friday Funday!')
-
 
 
 # 8-4 (Large Shirts)
@@ -25,13 +25,10 @@ make_shirt('I love Python!','medium')
 make_shirt('I love to go for walks!','small')
 
 
-
 # 8-5 (Cities)
 def describe_city(city,country='Michigan'):
-    print(f'{city} is in {country}')
+    print(f'\n{city} is in {country}')
+
 describe_city('Detroit')
 describe_city('Kalamazoo')
-describe_city('Paris','france')
-
-
-
+describe_city('Paris','France')
